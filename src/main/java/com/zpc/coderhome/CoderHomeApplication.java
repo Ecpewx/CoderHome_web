48a474d2e52e0409709c1ae11011b69310acef96
zhong-taishi <2414308993@qq.com>
2021-08-05 17:05:46 +0800

    initial

---
+++ src/main/java/com/zpc/coderhome/CoderHomeApplication.java
@@ -0,0 +1,13 @@
+package com.zpc.coderhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderHomeApplication.class, args);
+    }
+
+}
